Nexedi/erp5

commit 4b16e1dd391f2f840bf65f04d3911364df6393ae

use UnrestrictedMethod's super user instead of ad-hoc SUPER_USER. Squashed commit of the following:
commit 2ba8fb59b67cda4a35bda5ee809ac0dd6af40d84
Author: Kazuhiko Shiozaki <kazuhiko@nexedi.com>
Date:   Fri Jun 1 23:26:30 2012 +0200

    if the activity is called by super user, it should be invoked with the same permission as UnrestrictedMethod.

commit f63c2e8625934d0a5a056e933f4c7215098bfa1b
Author: Kazuhiko Shiozaki <kazuhiko@nexedi.com>
Date:   Fri Jun 1 15:58:41 2012 +0200

    use UnrestrictedMethod's super user instead of ad-hoc SUPER_USER.

commit 965460b092967bc3ada3ee7268e1f942fc770efd
Author: Kazuhiko Shiozaki <kazuhiko@nexedi.com>
Date:   Fri Jun 1 15:57:45 2012 +0200

    security query for super user should be simply empty.

commit 6d519b78f52f1a631d6663ee5594ae92a0730cc3
Author: Kazuhiko Shiozaki <kazuhiko@nexedi.com>
Date:   Fri Jun 1 15:37:33 2012 +0200

    support both ERP5Security's SUPER_USER and UnrestrictedMethod's super user.

commit 21431518b821a5e2756caad5393fc746bed79d36
Author: Kazuhiko Shiozaki <kazuhiko@nexedi.com>
Date:   Fri Jun 1 15:32:45 2012 +0200

    make sure that SUPER_USER can access the object explicitly, that can be required with erp5_web.

commit 63279ac74cbb40e520da36571927bfdee5af5e05
Author: Kazuhiko Shiozaki <kazuhiko@nexedi.com>
Date:   Fri Jun 1 15:27:00 2012 +0200

    use UnrestrictedMethod instead of ad-hoc SUPER_USER, still keeping SUPER_USER for compatibility.

product/CMFActivity/ActivityTool.py
@@ -44,6 +44,7 @@ from AccessControl.SecurityManagement import newSecurityManager
 from AccessControl.SecurityManagement import noSecurityManager
 from AccessControl.SecurityManagement import setSecurityManager
 from AccessControl.SecurityManagement import getSecurityManager
+from AccessControl.User import system as system_user
 from Products.CMFCore.utils import UniqueObject, _getAuthenticatedUser
 from Products.ERP5Type.Globals import InitializeClass, DTMLFile
 from Acquisition import aq_base, aq_inner, aq_parent
@@ -51,6 +52,8 @@ from ActivityBuffer import ActivityBuffer
 from ActivityRuntimeEnvironment import BaseMessage
 from zExceptions import ExceptionFormatter
 from BTrees.OIBTree import OIBTree
+from Zope2 import app
+from Products.ERP5Type.UnrestrictedMethod import PrivilegedUser
 
 try:
   from Products import iHotfix
@@ -248,6 +251,16 @@ class Message(BaseMessage):
     if user is None:
       uf = activity_tool.getPortalObject().aq_parent.acl_users
       user = uf.getUserById(user_name)
+    if user is None and user_name == system_user.getUserName():
+      # The following logic comes from unrestricted_apply()
+      # implementation in ERP5Type.UnrestrictedMethod.
+      try:
+        # XXX is it better to get roles from the parent (i.e. portal)?
+        uf = user.aq_inner.aq_parent
+      except AttributeError:
+        uf = app().acl_users
+      role_list = uf.valid_roles()
+      user = PrivilegedUser(user_name, None, role_list, ()).__of__(uf)
     if user is not None:
       user = user.__of__(uf)
       newSecurityManager(None, user)

product/ERP5Security/ERP5UserFactory.py
@@ -26,6 +26,7 @@ from Products.PluggableAuthService.interfaces.plugins import IUserFactoryPlugin
 from Products.PluggableAuthService.PropertiedUser import PropertiedUser
 from Products.PluggableAuthService.PropertiedUser import \
                                             _what_not_even_god_should_do
+from Products.ERP5Security.ERP5UserManager import SUPER_USER
 
 manage_addERP5UserFactoryForm = PageTemplateFile(
     'www/ERP5Security_addERP5UserFactory', globals(),
@@ -101,6 +102,10 @@ class ERP5User(PropertiedUser):
       As for getRolesInContext, we take into account _getAcquireLocalRoles for
       ERP5.
       """
+      if self.getUserName() == SUPER_USER:
+        # super user is allowed to accesss any object
+        return 1
+
       if object_roles is _what_not_even_god_should_do:
         return 0
 

product/ERP5Security/ERP5UserManager.py
@@ -28,6 +28,7 @@ from Products.PluggableAuthService.utils import classImplements
 from Products.PluggableAuthService.interfaces.plugins import IAuthenticationPlugin
 from Products.PluggableAuthService.interfaces.plugins import IUserEnumerationPlugin
 from Products.ERP5Type.Cache import CachingMethod, transactional_cached
+from Products.ERP5Type.UnrestrictedMethod import UnrestrictedMethod
 from ZODB.POSException import ConflictError
 import sys
 from DateTime import DateTime
@@ -126,6 +127,7 @@ class ERP5UserManager(BasePlugin):
         if login == SUPER_USER:
           return None
 
+        @UnrestrictedMethod
         def _authenticateCredentials(login, password, path,
           ignore_password=False):
             if not login or not (password or ignore_password):
@@ -138,9 +140,6 @@ class ERP5UserManager(BasePlugin):
 
             user = user_list[0]
 
-            sm = getSecurityManager()
-            if sm.getUser().getId() != SUPER_USER:
-              newSecurityManager(self, self.getUser(SUPER_USER))
             try:
               # get assignment
               assignment_list = [x for x in user.contentValues(portal_type="Assignment") if x.getValidationState() == "open"]
@@ -161,7 +160,7 @@ class ERP5UserManager(BasePlugin):
                      .getValidationState() != 'deleted': #user.getCareerRole() == 'internal':
                 return login, login # use same for user_id and login
             finally:
-              setSecurityManager(sm)
+              pass
             raise _AuthenticationFailure()
 
         _authenticateCredentials = CachingMethod(_authenticateCredentials,

product/ERP5/Document/Alarm.py
@@ -32,10 +32,11 @@ from DateTime import DateTime
 from AccessControl import ClassSecurityInfo, Unauthorized
 from AccessControl.SecurityManagement import getSecurityManager, \
             setSecurityManager, newSecurityManager
+from AccessControl.User import nobody
 from Products.CMFCore.utils import getToolByName
 from Products.ERP5Type import Permissions, PropertySheet
 from Products.ERP5Type.XMLObject import XMLObject
-from Products.ERP5Security.ERP5UserManager import SUPER_USER
+from Products.ERP5Type.UnrestrictedMethod import UnrestrictedMethod
 from Products.ERP5.mixin.periodicity import PeriodicityMixin
 
 class Alarm(XMLObject, PeriodicityMixin):
@@ -118,11 +119,10 @@ class Alarm(XMLObject, PeriodicityMixin):
       if not checkPermission(Permissions.ManagePortal, self):
         raise Unauthorized('fixing problems or activating a disabled alarm is not allowed')
 
-    # Switch to the superuser temporarily, so that the behavior would not
+    # Use UnrestrictedMethod, so that the behavior would not
     # change even if this method is invoked by random users.
-    sm = getSecurityManager()
-    newSecurityManager(None, portal_membership.getMemberById(SUPER_USER))
-    try:
+    @UnrestrictedMethod
+    def _activeSense():
       # Set the next date at which this method should be invoked
       self.setNextAlarmDate()
 
@@ -158,6 +158,13 @@ class Alarm(XMLObject, PeriodicityMixin):
           getattr(self.activate(tag=tag), method_id)()
         if self.isAlarmNotificationMode():
           self.activate(after_tag=tag).notify(include_active=True, params=params)
+
+    # switch to nobody temporarily so that unrestricted _activeSense
+    # is already invoked by system user.
+    sm = getSecurityManager()
+    newSecurityManager(None, nobody)
+    try:
+      _activeSense()
     finally:
       # Restore the original user.
       setSecurityManager(sm)

product/ERP5Catalog/CatalogTool.py
@@ -31,6 +31,7 @@ from Products.ZSQLCatalog.ZSQLCatalog import ZCatalog
 from Products.ZSQLCatalog.SQLCatalog import Query, ComplexQuery
 from Products.ERP5Type import Permissions
 from AccessControl import ClassSecurityInfo, getSecurityManager
+from AccessControl.User import system as system_user
 from Products.CMFCore.utils import UniqueObject, _getAuthenticatedUser, getToolByName
 from Products.ERP5Type.Globals import InitializeClass, DTMLFile
 from Acquisition import aq_base, aq_inner, aq_parent, ImplicitAcquisitionWrapper
@@ -391,7 +392,7 @@ class CatalogTool (UniqueObject, ZCatalog, CMFCoreCatalogTool, ActiveObject):
       """
       user = _getAuthenticatedUser(self)
       user_str = str(user)
-      user_is_superuser = (user_str == SUPER_USER)
+      user_is_superuser = (user == system_user) or (user_str == SUPER_USER)
       allowedRolesAndUsers = self._listAllowedRolesAndUsers(user)
       role_column_dict = {}
       local_role_column_dict = {}
@@ -508,6 +509,12 @@ class CatalogTool (UniqueObject, ZCatalog, CMFCoreCatalogTool, ActiveObject):
         values. The query takes into account the fact that some roles are
         catalogued with columns.
       """
+      user = _getAuthenticatedUser(self)
+      user_str = str(user)
+      user_is_superuser = (user == system_user) or (user_str == SUPER_USER)
+      if user_is_superuser:
+        # We need no security check for super user.
+        return query
       original_query = query
       security_uid_list, role_column_dict, local_role_column_dict = \
           self.getSecurityUidListAndRoleColumnDict(

product/ERP5Security/ERP5GroupManager.py
@@ -26,6 +26,7 @@ from Products.PluggableAuthService.interfaces.plugins import IGroupsPlugin
 from Products.ERP5Type.Cache import CachingMethod
 from Products.ERP5Type.ERP5Type \
   import ERP5TYPE_SECURITY_GROUP_ID_GENERATION_SCRIPT
+from Products.ERP5Type.UnrestrictedMethod import UnrestrictedMethod
 from Products.PluggableAuthService.PropertiedUser import PropertiedUser
 from ZODB.POSException import ConflictError
 
@@ -86,17 +87,13 @@ class ERP5GroupManager(BasePlugin):
     if principal.getId() == SUPER_USER:
       return ()
 
+    @UnrestrictedMethod
     def _getGroupsForPrincipal(user_name, path):
       security_category_dict = {} # key is the base_category_list,
                                   # value is the list of fetched categories
       security_group_list = []
       security_definition_list = ()
 
-      # because we aren't logged in, we have to create our own
-      # SecurityManager to be able to access the Catalog
-      sm = getSecurityManager()
-      if sm.getUser().getId() != SUPER_USER:
-        newSecurityManager(self, self.getUser(SUPER_USER))
       try:
         # To get the complete list of groups, we try to call the
         # ERP5Type_getSecurityCategoryMapping which should return a list
@@ -180,7 +177,7 @@ class ERP5GroupManager(BasePlugin):
                   generator_name,
                   error = sys.exc_info())
       finally:
-        setSecurityManager(sm)
+        pass
       return tuple(security_group_list)
 
     if not NO_CACHE_MODE:

product/ERP5Security/ERP5KeyAuthPlugin.py
@@ -48,6 +48,7 @@ from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
 from Products.PluggableAuthService.plugins.CookieAuthHelper import CookieAuthHelper
 
 from Products.ERP5Type.Cache import CachingMethod
+from Products.ERP5Type.UnrestrictedMethod import UnrestrictedMethod
 from Products.ERP5Security.ERP5UserManager import ERP5UserManager,\
                                                   SUPER_USER,\
                                                   _AuthenticationFailure
@@ -327,6 +328,7 @@ class ERP5KeyAuthPlugin(ERP5UserManager, CookieAuthHelper):
         return None
 
       #Function to allow cache
+      @UnrestrictedMethod
       def _authenticateCredentials(login):
         if not login:
           return None
@@ -337,10 +339,7 @@ class ERP5KeyAuthPlugin(ERP5UserManager, CookieAuthHelper):
           raise _AuthenticationFailure()
         user = user_list[0]
 
-        #We need to be super_user
-        sm = getSecurityManager()
-        if sm.getUser().getId() != SUPER_USER:
-          newSecurityManager(self, self.getUser(SUPER_USER))
+        if True:
           try:
             # get assignment list
             assignment_list = [x for x in user.contentValues(portal_type="Assignment") \
@@ -361,7 +360,7 @@ class ERP5KeyAuthPlugin(ERP5UserManager, CookieAuthHelper):
             if len(valid_assignment_list) > 0:
               return (login, login)
           finally:
-            setSecurityManager(sm)
+            pass
 
           raise _AuthenticationFailure()
 

jgpjuniorj/j

commit b7c85b0f7ee415e73ff177534c52e1017526b5e5

make 'System Processes' a virtual ERP5 user.
This commit fixes the regression in 4b16e1dd39 where python scripts
generated by alarm, that can happen with upgrader, will be executed as
Anonymous because it's owner does not exist.

product/CMFActivity/ActivityTool.py
@@ -245,22 +245,23 @@ class Message(BaseMessage):
     portal = activity_tool.getPortalObject()
     portal_uf = portal.acl_users
     uf = portal_uf
-    user = uf.getUserById(user_name)
-    # if the user is not found, try to get it from a parent acl_users
-    # XXX this is still far from perfect, because we need to store all
-    # information about the user (like original user folder, roles) to
-    # replay the activity with exactly the same security context as if
-    # it had been executed without activity.
-    if user is None:
-      uf = portal.aq_parent.acl_users
-      user = uf.getUserById(user_name)
-    if user is None and user_name == system_user.getUserName():
+    if user_name == system_user.getUserName():
       # The following logic partly comes from unrestricted_apply()
       # implementation in ERP5Type.UnrestrictedMethod but we get roles
       # from the portal to have more roles.
       uf = portal_uf
       role_list = uf.valid_roles()
       user = PrivilegedUser(user_name, None, role_list, ()).__of__(uf)
+    else:
+      user = uf.getUserById(user_name)
+      # if the user is not found, try to get it from a parent acl_users
+      # XXX this is still far from perfect, because we need to store all
+      # information about the user (like original user folder, roles) to
+      # replay the activity with exactly the same security context as if
+      # it had been executed without activity.
+      if user is None:
+        uf = portal.aq_parent.acl_users
+        user = uf.getUserById(user_name)
     if user is not None:
       user = user.__of__(uf)
       newSecurityManager(None, user)

product/ERP5Security/ERP5UserFactory.py
@@ -18,6 +18,7 @@
 from Products.ERP5Type.Globals import InitializeClass
 from Acquisition import aq_inner, aq_parent
 from AccessControl import ClassSecurityInfo
+from AccessControl.User import system as system_user
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from App.config import getConfiguration
 from Products.PluggableAuthService.plugins.BasePlugin import BasePlugin
@@ -102,7 +103,7 @@ class ERP5User(PropertiedUser):
       As for getRolesInContext, we take into account _getAcquireLocalRoles for
       ERP5.
       """
-      if self.getUserName() == SUPER_USER:
+      if self.getUserName() in (SUPER_USER, system_user.getUserName()):
         # super user is allowed to accesss any object
         return 1
 

product/ERP5Security/ERP5UserManager.py
@@ -20,6 +20,7 @@ from AccessControl import ClassSecurityInfo
 from AccessControl.AuthEncoding import pw_validate
 from AccessControl.SecurityManagement import getSecurityManager,\
     setSecurityManager, newSecurityManager
+from AccessControl.User import system as system_user
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from Products.PluggableAuthService.PluggableAuthService import \
     _SWALLOWABLE_PLUGIN_EXCEPTIONS
@@ -124,7 +125,7 @@ class ERP5UserManager(BasePlugin):
           login = credentials.get('external_login')
           ignore_password = True
         # Forbidden the usage of the super user.
-        if login == SUPER_USER:
+        if login in (SUPER_USER, system_user.getUserName()):
           return None
 
         @UnrestrictedMethod
@@ -224,9 +225,9 @@ class ERP5UserManager(BasePlugin):
 
         id_list = []
         for user_id in id:
-          if SUPER_USER == user_id:
-            info = { 'id' : SUPER_USER
-                    , 'login' : SUPER_USER
+          if user_id in (SUPER_USER, system_user.getUserName()):
+            info = { 'id' : user_id
+                    , 'login' : user_id
                     , 'pluginid' : plugin_id
                     }
             user_info.append(info)

product/ERP5Security/tests/testERP5Security.py
@@ -219,6 +219,16 @@ class TestUserManagement(ERP5TypeTestCase):
     from Products.ERP5Security.ERP5UserManager import SUPER_USER
     self._assertUserDoesNotExists(SUPER_USER, '')
 
+  def test_PersonWithSystemProcessesLoginCannotBeCreated(self):
+    """Tests one cannot create person with the "super user" special login."""
+    from AccessControl.User import system as system_user
+    self.assertRaises(RuntimeError, self._makePerson, reference=system_user.getUserName())
+
+  def test_PersonWithSystemProcessesLogin(self):
+    """Tests one cannot use the "super user" special login."""
+    from AccessControl.User import system as system_user
+    self._assertUserDoesNotExists(system_user.getUserName(), '')
+
   def test_searchUsers(self):
     p1 = self._makePerson(reference='person1')
     p2 = self._makePerson(reference='person2')
@@ -401,6 +411,30 @@ class TestUserManagement(ERP5TypeTestCase):
     person.setReference(None)
     self.tic()
     self.assertEqual(None, person.getReference())
+
+  def test_scriptGenerationByAlarm(self):
+    """Make sure a script generated by alarm can be executed"""
+    self.portal = self.getPortal()
+    # create a security configuration script
+    skin_folder = self.portal.portal_skins.custom
+    if 'test_python_script' not in skin_folder.objectIds():
+      createZODBPythonScript(
+        skin_folder, 'test_alarm_script', '',
+        """
+script_id='test_python_script'
+script_params=''
+script_content='return context.getPortalObject().person_module.newContent().getId()'
+if getattr(container, script_id, None) is None:
+  container.manage_addProduct['PythonScripts'].manage_addPythonScript(id=script_id)
+  script = container[script_id]
+  script.ZPythonScript_edit(script_params, script_content)
+        """)
+    alarm = self.portal.portal_alarms.newContent(
+      active_sense_method_id='test_alarm_script'
+      )
+    alarm.activeSense()
+    self.tic()
+    self.assertTrue(self.portal.test_python_script() is not None)
 
 class TestUserManagementExternalAuthentication(TestUserManagement):
   def getTitle(self):